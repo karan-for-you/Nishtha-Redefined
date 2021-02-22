4b15825e75c228f061538b0e5c835f1de925a367
Karan <contactkaran013@gmail.com>
2021-02-22 17:18:35 +0530

    Random Progress in Contact Dialog

--- app/src/main/java/com/karan/nishtharedefined/ui/dialog/ContactDebugDialog.kt
+++ app/src/main/java/com/karan/nishtharedefined/ui/dialog/ContactDebugDialog.kt
@@ -1,4 +1,20 @@
 package com.karan.nishtharedefined.ui.dialog
 
-class ContactDebugDialog {
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.Window
+import com.karan.nishtharedefined.R
+
+class ContactDebugDialog(context: Context) : Dialog(context){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.contact_debug_dialog)
+    }
+
+    fun initViews(){
+
+    }
 }
