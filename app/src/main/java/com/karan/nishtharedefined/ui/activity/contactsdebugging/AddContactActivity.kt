c0e63451e0b9e160cc2bea3e658659567f327437
Karan <contactkaran013@gmail.com>
2021-02-26 10:17:34 +0530

    Dialog By Passing-ViewModel Fight

---
+++ app/src/main/java/com/karan/nishtharedefined/ui/activity/contactsdebugging/AddContactActivity.kt
@@ -0,0 +1,13 @@
+package com.karan.nishtharedefined.ui.activity.contactsdebugging
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.karan.nishtharedefined.R
+
+class AddContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+    }
+}
