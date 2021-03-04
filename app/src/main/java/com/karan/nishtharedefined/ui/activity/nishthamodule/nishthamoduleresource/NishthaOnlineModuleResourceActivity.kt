e811eb7f07a83581f07b37de8b99c586bdfc90b5
Karan <contactkaran013@gmail.com>
2021-03-04 16:22:27 +0530

    Module Resource Activity Progress

--- app/src/main/java/com/karan/nishtharedefined/ui/activity/nishthamodule/nishthamoduleresource/NishthaOnlineModuleResourceActivity.kt
+++ app/src/main/java/com/karan/nishtharedefined/ui/activity/nishthamodule/nishthamoduleresource/NishthaOnlineModuleResourceActivity.kt
@@ -3,13 +3,21 @@ package com.karan.nishtharedefined.ui.activity.nishthamodule.nishthamoduleresour
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.karan.nishtharedefined.R
 import com.karan.nishtharedefined.databinding.ActivityNishthaOnlineModuleResourceBinding
+import com.karan.nishtharedefined.model.nishthaonline.NishthaModuleModel
 
 class NishthaOnlineModuleResourceActivity : AppCompatActivity() {
 
     private lateinit var bindingNishthaOnlineModuleResourceActivity :
             ActivityNishthaOnlineModuleResourceBinding
+    private val nishthaOnlineModuleResourceViewModel by lazy {
+        ViewModelProvider(this,NishthaOnlineModuleResourceViewModel.Factory(application))
+            .get(NishthaOnlineModuleResourceViewModel::class.java)
+    }
+
+    private var moduleInfo : NishthaModuleModel? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +26,13 @@ class NishthaOnlineModuleResourceActivity : AppCompatActivity() {
             this,
             R.layout.activity_nishtha_online_module_resource
         )
+        receiveModuleInfo()
     }
+
+    private fun receiveModuleInfo(){
+        moduleInfo = intent.extras?.get("moduleInfo") as NishthaModuleModel
+    }
+
+
+
 }
--- app/src/main/java/com/karan/nishtharedefined/ui/activity/nishthamodule/nishthamoduleresource/NishthaOnlineModuleResourceViewModel.kt
+++ app/src/main/java/com/karan/nishtharedefined/ui/activity/nishthamodule/nishthamoduleresource/NishthaOnlineModuleResourceViewModel.kt
@@ -1,4 +1,47 @@
 package com.karan.nishtharedefined.ui.activity.nishthamodule.nishthamoduleresource
 
-class NishthaOnlineModuleResourceViewModel {
+import android.app.Application
+import androidx.lifecycle.*
+import com.karan.nishtharedefined.model.nishthaonline.NishthaOnlineModuleResourceModel
+import com.karan.nishtharedefined.networking.ServiceBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class NishthaOnlineModuleResourceViewModel(app : Application) : AndroidViewModel(app) {
+
+
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var _moduleResourceList = MutableLiveData<ArrayList<NishthaOnlineModuleResourceModel>>()
+    val moduleResourceList : LiveData<ArrayList<NishthaOnlineModuleResourceModel>>
+        get() = _moduleResourceList
+
+    private fun getModuleResources(lang:String, catId : String){
+        val service = ServiceBuilder.retrofitService.getOnlineResourceDetailAsync(
+            lang = lang,
+            cat_id = catId
+        )
+        uiScope.launch {
+            try{
+                _moduleResourceList.value = service.await()
+            }catch (e : Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    @Suppress("UNCHECKED_CAST")
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(NishthaOnlineModuleResourceViewModel::class.java))
+                return NishthaOnlineModuleResourceViewModel(app) as T
+            throw IllegalAccessException("Can't create Nishtha Online Module Resource View Model")
+        }
+
+    }
+
 }
